2bbc14e9a5c4cddc621b3c049a7929a677968676
Diego Oliveira da Silva <diegolinkk@gmail.com>
2021-05-11 21:56:24 -0300

    Revert "Exercicios do 01 ao 10 da estrutura sequencial"
    This reverts commit d57b61093ff2dbd9eaa6100e2c75132ea3675285.

--- estrutura-sequencial/exercicio05.py
+++
@@ -1,6 +0,0 @@
-#Faça um Programa que converta metros para centímetros.
-
-metros = int(input("Digite o tamanho em metros: "))
-centimetros = metros * 100
-
-print("O valor em centímetros é {}".format(centimetros))
--- estrutura-sequencial/exercicio06.py
+++
@@ -1,5 +0,0 @@
-#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-raio = int(input("Digite o raio do círculo: "))
-area = 3.14 * (raio ** 2)
-
-print("A área do círculo é: {}".format(area))
